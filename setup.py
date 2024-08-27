5b1c0b12e6ece8e3ebe8e6a93e4c06726da76b38
luigilopesz <luigilopes09@gmail.com>
2024-08-26 22:59:08 -0300

    a

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="alglinPYGAME",  # Nome do pacote
-    version="0.1.0",  # Versão do pacote
-    packages=find_packages(include=['alglinPYGAME', 'alglinPYGAME.*']),  # Encontrar automaticamente todos os pacotes
-    install_requires=[  # Dependências
-        'pygame',  # Adicione dependências como pygame aqui
-        'numpy',   # Adicione numpy ou outras bibliotecas que você usa
+    name="alglinPYGAME",
+    version="0.1.0",
+    packages=find_packages(),  # Encontrar automaticamente todos os pacotes
+    install_requires=[
+        'pygame',
+        'numpy',
     ],
     package_data={
-        'alglinPYGAME': ['assets/**/*.png']  # Inclui todos os arquivos .png dentro da pasta 'assets'
+        'alglinPYGAME': ['assets/**/*.png']
     },
     entry_points={
         "console_scripts": [
-            "alglinPYGAME=alglinPYGAME.main:main",  # Corrigido o caminho do entry point
+            "alglinPYGAME=alglinPYGAME.main:main",  # Certifique-se de que este caminho esteja correto
         ],
     },
-    author="Luigi Lopes",  # Seu nome
-    author_email="luigilopes09@gmail.com",  # Seu email
+    author="Luigi Lopes",
+    author_email="luigilopes09@gmail.com",
     description="Jogo de Algebra Linear",
-    long_description=open("README.md").read(),  # Descrição longa (usualmente do README)
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/luigilopesz/alglinPYGAME",  # URL do seu repositório
+    url="https://github.com/luigilopesz/alglinPYGAME",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Versão mínima do Python
+    python_requires='>=3.6',
 )
